c3b12ceb929e969503c8e23c0d5df3d507af94b7
grosseRiese <49767507+grosseRiese@users.noreply.github.com>
2020-03-17 00:35:59 +0100

    modifier
    js,css

--- js/App.js
+++ js/App.js
@@ -4,13 +4,20 @@ const YOUR_ACCESS_KEY = MY_ACCESS_KEY;
 const url = 'https://api.unsplash.com/search/photos?page=1&query=london&client_id='+YOUR_ACCESS_KEY;
 
 function loopData(results){
-   results.forEach(element => {
-         let resultsItemImg = document.createElement('li');
-         resultsItemImg.innerHTML ='<figure><img src="'+ element.urls.full +'" alt="london"> <figcaption> <h3>'
-         + element.likes +' <span>❤</span></h3></figcaption>' 
-                 + '</figure></li>';
-        ulElem.append(resultsItemImg );
-     });
+
+        results.forEach(element => {
+            let resultsItemImg = document.createElement('li');
+            resultsItemImg.innerHTML = '<figure><img src="'+ element.urls.full +'" alt="london"> <figcaption>'
+                                        + '<h3>' + element.likes +' <span>❤</span></h3>'
+                                        + '</figcaption>'
+                                        + '<article class="overlay"> <p class="text">' + element.description
+                                        + '<br><span><br><i>'+ element.user.name +'</i></span></p> </article>' 
+                                        + '</figure>'
+                                        + '</li>';
+
+            ulElem.append(resultsItemImg );
+        });
+
 }
 
 function getImages() {
